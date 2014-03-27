35effaab78f922b87c2c660afed3e28e15283180
Loon <ppgl1988@gmail.com>
2014-03-28 00:20:51 +0800

    Initial commit

--- src/com/poitest/RecordsetPlugin.java
+++ src/com/poitest/RecordsetPlugin.java
@@ -177,4 +177,20 @@ public final class RecordsetPlugin implements Interceptor {
 
         this.properties = properties;
     }
+
+    public String getLimitString(String sql, int offset, int limit) {
+
+        sql = sql.trim();
+
+        StringBuffer pagingSelect = new StringBuffer(sql.length() + 100);
+
+        pagingSelect.append("select * from ( select row_.*, rownum rownum_ from ( ");
+
+        pagingSelect.append(sql);
+
+        pagingSelect.append(" ) row_ ) where rownum_ >= " + offset + " and rownum_ <= " + (offset + limit - 1));
+
+
+        return pagingSelect.toString();
+    }
 }
